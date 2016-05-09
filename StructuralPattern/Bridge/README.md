da68a7508957319db68ef21bb15cdd12d2f76745
ningjin <ningjin@news18a.com>
2016-05-09 11:38:55 +0800

    add adapter pattern

---
+++ StructuralPattern/Bridge/README.md
@@ -0,0 +1,3 @@
+##桥接模式
+适用于当一个系统中某个抽象类或接口包含的子类太多的情况  
+![适配模式](https://sourcemaking.com/files/v2/content/patterns/Bridge_example-2x.png)
---
+++ StructuralPattern/Bridge/bridge_Bridge.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * 桥接模式
+ * 
+ * 还需要理解下 这个例子举得不太好
+ */
+abstract class BridgeBook 
+{
+	private $author;
+	private $title;
+	private $showType;
+
+	public function __construct($author, $title, $showType)
+	{
+		$this->author = $author;
+		$this->title = $title;
+
+		if( 'cap' == $showType )
+			$this->showType = new CapBook();
+		else
+			$this->showType = new StarBook();
+	}
+
+	public function showAuthor()
+	{
+		return $this->showType->showAuthor($this->author);
+	}
+
+	public function showTitle()
+	{
+		return $this->showType->showTitle($this->title);
+	}
+}
+
+class BridgeAuthorTitleBook extends BridgeBook
+{
+	public function showAuthorTitle()
+	{
+		echo $this->showAuthor() . "\'s " . $this->showTitle();
+	}
+}
+
+class BridgeTitleAuthorBook extends BridgeBook
+{
+	public function showTitleAuthor()
+	{
+		echo $this->showTitle() . " of " . $this->showAuthor();
+	}
+}
+
+
+abstract class Book
+{
+	abstract function showAuthor($author);
+	abstract function showTitle($title);
+}
+
+class CapBook extends Book
+{
+	public function showAuthor($author)
+	{
+		return ucwords($author);
+	}
+
+	public function showTitle($title)
+	{
+		return ucwords($title);
+	}
+}
+
+class StarBook extends Book
+{
+	public function showAuthor($author)
+	{
+		return '**' . $author . '**';
+	}
+
+	public function showTitle($title)
+	{
+		return '**' . $title . '**';
+	}
+}
+
+$titleAuthor = new BridgeTitleAuthorBook('ningjin', 'programming', 'cap');
+$titleAuthor->showTitleAuthor();
