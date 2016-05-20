8178ab169b54b886bc248a0d6cdac54974d84935
ningjin <ningjin@news18a.com>
2016-05-20 11:22:55 +0800

    edit complete flyweight

--- StructuralPattern/Flyweight/Flyweight_CompelteWeight.php
+++ StructuralPattern/Flyweight/Flyweight_CompelteWeight.php
@@ -40,8 +40,72 @@ class Coordinates {
 //棋子制造工厂
 class IgoChessmanFactory
 {
-	private $IgoChessmanFactory;
-	private $hashTable;
+	private static $IgoChessmanFactory;
+	private static $hashTable = array();
 
-	
+	public function __construct()
+	{
+		$whiteIgoChess = new WhiteChessman;
+		self::$hashTable['w'] = $whiteIgoChess;
+		$blackIgoChess = new BlackChessman;
+		self::$hashTable['b'] = $blackIgoChess;
+	}
+
+	//单例模式
+	public static function getInstance()
+	{
+		if ( !self::$IgoChessmanFactory )
+			self::$IgoChessmanFactory = new IgoChessmanFactory;
+		return self::$IgoChessmanFactory;
+	}
+
+	//生产棋子
+	public function getIgoChessman($color)
+	{
+		return self::$hashTable[$color];
+	}
 }
+
+//棋子类 抽象
+abstract class IgoChessman
+{
+	abstract public function getColor();	//棋子颜色
+	//渲染棋子
+	public function display(Coordinates $Coordinates)
+	{
+		echo '棋子颜色' . $this->getColor() . '，位置x：' . $Coordinates->getX() . '，位置y：' . $Coordinates->getY() ;
+	}
+}
+
+//白色棋子 具体类
+class WhiteChessman extends IgoChessman
+{
+	public function getColor()
+	{
+		return '白色';
+	}
+}
+
+class BlackChessman extends IgoChessman
+{
+	public function getColor()
+	{
+		return '黑色';
+	}
+}
+
+//实例化工厂
+$IgoChessmanFactory = IgoChessmanFactory::getInstance();
+
+//生产白色棋子
+$WhiteChessman = $IgoChessmanFactory->getIgoChessman('w');
+$WhiteChessman->display(new Coordinates('1','2'));
+
+$WhiteChessmana = $IgoChessmanFactory->getIgoChessman('w');
+$WhiteChessmana->display(new Coordinates('1','2'));
+
+echo '<p>';
+
+//生产黑色棋子
+$BlackChessman = $IgoChessmanFactory->getIgoChessman('b');
+$BlackChessman->display(new Coordinates('5','6'));
